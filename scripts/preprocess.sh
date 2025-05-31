8fb055943b92392eb2422ecc99bc509e4b73a702
Dnau15 <15dnau@gmail.com>
2025-05-31 14:36:27 +0300

    upd: add reqs

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,4 +1,5 @@
 ./download_test_dataset.sh
 ./download_model.sh
+pip install -r requirements.txt
 huggingface-cli download Dnau15/EvalMuseMini --repo-type dataset --local-dir ./data/dataset
 python data/preprocessing_v2.py
