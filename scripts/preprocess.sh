47994864f80544558f99b6abda7032304d8549c7
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2025-06-01 19:03:24 +0300

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,7 @@
+echo "START MODEL DOWNLOADING"
 scripts/download_test_dataset.sh
 scripts/download_model.sh
 pip install -r requirements.txt
+echo "START MODEL DOWNLOADING EVALMUSEMINI"
 huggingface-cli download Dnau15/EvalMuseMini --repo-type dataset --local-dir ./data/dataset
 python data/preprocessing_v2.py
